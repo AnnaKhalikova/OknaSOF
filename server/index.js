64a9e3a0016e121fbe4ba49c848a1561921cc9cb
ICX\hanna.khalikava <hanna.khalikova@gmail.com>
2023-10-06 23:05:55 +0300

    update server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,40 @@
-const express = require('express')
-const app = express()
-const port = 3000
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+const ViberBot = require('viber-bot').Bot;
+const BotEvents = require('viber-bot').Events;
+const TextMessage = require('viber-bot').Message.Text;
+const winston = require('winston');
+const express = require('express');
+const app = express();
+const config = require('./config.json');
+
+let logger = winston.createLogger({
+    exitOnError: false,
+    transports: [
+        new (winston.transports.Console)(),
+        new (winston.transports.File)({ filename: 'app.log'})]
 })
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+const bot = new ViberBot({
+    logger: logger,
+    authToken: config.token,
+    name: config.name,
+    avatar: config.avatar
+});
+
+bot.on(BotEvents.SUBSCRIBED, response => {
+    response.send(new TextMessage(`Hi ${response.userProfile.name}, my name is ${bot.name}!`));
+});
+bot.on(BotEvents.MESSAGE_RECEIVED, (message, response) => {
+    response.send(new TextMessage(`I have received the following message: ${message}`));
+});
+// Bind the bot middleware to the app instance
+const webhookUrl = "https://webhook.site/0ac43896-99be-4f5b-af31-d7feeccf73b4/viber/webhook";
+app.use('/viber/webhook', bot.middleware());
+
+// Webhook will be used for receiving callbacks and user messages from Viber
+app.listen(config.port, () => {
+    logger.info(`Application is running! Port: ${config.port}`);
+    logger.info('expose_domain:' + config.expose_domain + ' expose_uri_pass:' + config.expose_uri_path);
+    bot.setWebhook(config.expose_domain + config.expose_uri_path).catch(error => {
+        logger.debug(`Error: The webhook ${config.expose_domain + config.expose_uri_path} cannot be set. ${error}`);
+        process.exit(1);
+    });
+});
